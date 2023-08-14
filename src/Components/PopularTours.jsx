9c0b093a6a8646e609dcf4f96b184d862925f5bb
Notjessand <jessennorom@gmail.com>
2023-08-14 08:13:52 -0500

    fifth commit

--- src/Components/PopularTours.jsx
+++ src/Components/PopularTours.jsx
@@ -4,7 +4,7 @@ import ToursCarosel from "./ToursCarosel";
 const PopularTours = () => {
   return (
     <div className="h-[100vh] flex flex-col justify-center md:pb-32 bg-gray-900">
-      <p className="text-white text-3xl text-center mt-5">Popular Tours</p>
+      <p className="text-white text-3xl text-center md:pt-12">Popular Tours</p>
       <p className="text-white text-center mt-2">
         From Majestic Mountains to Pristine Beaches – Our Picks for You
       </p>
