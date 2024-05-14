0cdceac7728464d499f1cabdb19e94b2891ef68a
Notjessand <jessennorom@gmail.com>
2024-05-14 05:04:29 -0500

    additional updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,44 @@
 "use client";
-import React from "react";
+import React,{useState, useEffect} from "react";
 import Hero from "@/components/Hero";
 import PopularTours from "@/components/PopularTours";
 import Discover from "@/components/Discover";
 
 export default function Page() {
+   const [imagesLoaded, setImagesLoaded] = useState(false);
+  useEffect(() => {
+    const handleImageLoad = () => {
+      // Set imagesLoaded to true when all images have loaded
+      setImagesLoaded(true);
+    };
+
+    // Listen for the load event on all images
+    window.addEventListener("load", handleImageLoad);
+
+    // Clean up event listener
+    return () => {
+      window.removeEventListener("load", handleImageLoad);
+    };
+  }, []);
+
+  setTimeout(() => {
+    setImagesLoaded(true)
+  }, 6000);
+
   return (
     <div className="bg-slate-900 my-0 py-0 font-custom">
-      <Hero />
-      <PopularTours />
-      <Discover />
+      {!imagesLoaded && (
+        <div className="flex justify-center items-center h-screen transition-all duration-500">
+          <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-white"></div>
+        </div>
+      )}
+      {imagesLoaded && (
+        <>
+          <Hero />
+          <PopularTours />
+          <Discover />
+        </>
+      )}
     </div>
   );
 }
