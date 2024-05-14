4a7c26f5a014fe2cf761c187f0fd8a593a3b947c
Notjessand <jessennorom@gmail.com>
2024-05-14 01:43:40 -0500

    additional updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import Hero from "@/components/Hero";
-
 import PopularTours from "@/components/PopularTours";
 import Discover from "@/components/Discover";
 
