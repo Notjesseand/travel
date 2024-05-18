d98b32783a1b1c453bc0a90177eaad88f0c68d82
Notjessand <jessennorom@gmail.com>
2024-05-18 00:37:43 -0500

    addition of destinations dynamic routing

--- app/destinations/[id]/page.tsx
+++ app/destinations/[id]/page.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import { fetchCityData } from "@/api/fetchCity";
 
-const page = ({ params }: { params: any }) => {
-  const data = params.id;
+export default async function Page({ params }: { params: any }) {
+  const { id } = params;
+
+  // Replace hyphens with spaces
+  const searchParams = id.replace(/-/g, " ");
+  fetchCityData(searchParams);
+
+  const data = await fetchCityData(searchParams);
   console.log(params);
+  console.log(data);
 
-  return <div>{data}</div>;
-};
-
-export default page;
+  return (
+    <main>
+      <div className="h-96 bg-pink-400"></div>
+      <div className="gap-10  text-lg mt-3 px-16 py-8">
+        <span className="gap-4 text-3xl">
+          {/* <span className="px-2">City:</span> */}
+          {data[0].name}
+        </span>
+        <p>{data[0].country}</p>
+        <p>Population: {data[0].population}</p>
+      </div>
+    </main>
+  );
+}
